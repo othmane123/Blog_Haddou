ac4225a649eea5b8c859b2c33e2d4001729ba2cf
othmane HADDOU <othmhad@gmail.com>
2013-01-29 23:55:57 +0100

    version 2

--- HADDOU Blog en local/UE33/supprimer_article.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-include('includes/connexion.inc.php');
-include('includes/fonctions.inc.php');
-$id =(int)var_get('id');
-if($id) requete_notif("DELETE FROM article WHERE id='$id'",'article','supprimé');//supprime l'article et confirme le succès par une notification
-header('Location:index.php');
